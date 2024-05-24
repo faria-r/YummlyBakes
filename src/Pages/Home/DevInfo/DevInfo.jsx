0b287461f2d2ca722b8633826ac2da0b24e23652
faria-r <fariacse11@gmail.com>
2024-05-24 13:52:13 +0600

    Updated dev info

--- src/Pages/Home/DevInfo/DevInfo.jsx
+++ src/Pages/Home/DevInfo/DevInfo.jsx
@@ -5,8 +5,20 @@ const DevInfo = () => {
   return (
     <div className="w-auto  p-6 text-center my-16 ">
       <div className="w-[80vw] mx-auto lg:flex justify-center items-center text-center p-6">
-        <div className="mr-16 w-52 mx-auto">
-          <img className="" src={fr} alt="" />
+        <div className="mr-16 mx-auto">
+          <img className=" w-52 h-[46vh]" src={fr} alt="" />
+          <div className=" text-start mt-6">
+              <p className="font-bold mb-2 ">Experience</p>
+              <p className="">
+              <span className="font-bold"> Instructor</span> <br />
+             <span className="text-xs leading-none">  Department of </span> <br />
+                <span className="font-mono font-bold">
+                  Computer Science & Technology
+                </span>
+              </p>
+              <p className="">Compact Polytechnic Institute</p>
+              <p className=""> June <span className="font-mono font-semibold">2020</span> - Present</p>
+            </div>
         </div>
         <div className="text-center lg:text-start w-[50vw] mx-auto pt-6 lg:pt-2">
           <h2 className="text-2xl font-mono">Faria Rahman</h2>
@@ -83,8 +95,10 @@ const DevInfo = () => {
               <p className="">Graduation Year : 2023</p>
               <p className="">CGPA : 3.84</p>
             </div>
+          
           </div>
-          <div className=" pt-6  w-[30vw] my-4 ">
+       
+          <div className=" pt-6  w-[30vw] lg:mt-16 mb-4 ">
             <div>
               <button className="btn border-2 border-black hover:bg-black hover:text-white text-black bg-transparent rounded-none px-20 py-4 mb-8 text-center ">
                 CONTACT ME

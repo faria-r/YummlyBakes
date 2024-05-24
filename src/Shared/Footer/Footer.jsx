e442ca847a9febec3784c7d299cb5180d842557e
faria-r <fariacse11@gmail.com>
2024-05-24 12:05:34 +0600

    Navbar designed

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -7,16 +7,18 @@ import {
 } from "react-icons/md";
 import { PiPhoneDisconnectBold } from "react-icons/pi";
 import SocialIcon from "../../Components/SocialIcon/SocialIcon";
-import logo from '../../assets/r.png'
-import logo1 from '../../assets/j.png'
-import logo2 from '../../assets/g.png'
+import logo from "../../assets/r.png";
+import logo1 from "../../assets/j.png";
+import logo2 from "../../assets/g.png";
 const Footer = () => {
   return (
     <div className="w-[90vw] mx-auto mt-8 pt-8 pb-24 ">
-      <h2 className="text-3xl my-6 bg-gradient-to-r from-orange-500 via-green-600 to-yellow-400 inline-block text-transparent bg-clip-text">Yummly Bakes - Recipes</h2>
+      <h2 className="text-3xl my-6 bg-gradient-to-r from-orange-500 via-green-600 to-yellow-400 inline-block text-transparent bg-clip-text">
+        Yummly Bakes - Recipes
+      </h2>
       <div className="grid grid-cols-1 lg:grid-cols-3 w-[80vw] p-6  items-start  font-sans">
         <div>
-          <h2 className="text-xl lg:mt-6">Share Your Recipes With Us</h2>
+          <h2 className="text-xl lg:mt-6 font-mono">Share Your Recipes With Us</h2>
         </div>
         <div className="flex-col justify-center font-bold">
           <div className="lg:flex justify-around items-center my-6">
@@ -37,7 +39,6 @@ const Footer = () => {
               02346 JOHNSBARG UTC- CITY RY
             </p>
           </div>
-          <p className="lg:ml-[2%]">QUICK LINKS</p>
         </div>
         <div className="lg:ml-[10%] lg:flex justify-around items-center my-6 font-bold">
           <p className="text-3xl -mr-16">
@@ -50,15 +51,21 @@ const Footer = () => {
         </div>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 p-4 w-[90vw] ">
-        <div className="text-2xl bg-transparent"><img src={logo} className=" w-16 bg-transparent"alt="" /></div>
+        <div className="text-2xl bg-transparent">
+          <img src={logo} className=" w-16 bg-transparent" alt="" />
+        </div>
         <div className="lg:-ml-[38%]">
           <h2 className="my-4">
             All information deemed reliable but not guaranteed and should be
             independently reviewed and verified.
           </h2>
           <div className="flex justify-start text-4xl gap-4">
-            <div><img className="w-8" src={logo1} alt="" srcset="" /></div>
-            <div><img className="w-8" src={logo2} alt="" srcset="" /></div>
+            <div>
+              <img className="w-8" src={logo1} alt="" srcset="" />
+            </div>
+            <div>
+              <img className="w-8" src={logo2} alt="" srcset="" />
+            </div>
           </div>
         </div>
       </div>
@@ -66,14 +73,21 @@ const Footer = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-2 mt-6 gap-24 lg:gap-4">
         <div className="text-2xl relative">
-            <h2>Powered By <span className="font-bold underline bg-gradient-to-r from-orange-500 via-green-600 to-yellow-400 inline-block text-transparent bg-clip-text">Yummly Bakes</span></h2>
-            <div className="absolute left-0">
+          <h2>
+            Powered By{" "}
+            <span className="font-bold underline bg-gradient-to-r from-orange-500 via-green-600 to-yellow-400 inline-block text-transparent bg-clip-text">
+              Yummly Bakes
+            </span>
+          </h2>
+          <div className="absolute left-0">
             <SocialIcon></SocialIcon>
-            </div>
-            
+          </div>
         </div>
         <div>
-            <p className="text-2xl">Copyright © 2024 <span className="font-bold"> | Privacy Policy</span></p>
+          <p className="text-2xl">
+            Copyright © 2024{" "}
+            <span className="font-bold"> | Privacy Policy</span>
+          </p>
         </div>
       </div>
     </div>
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import logo from '../../assets/r.png'
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
@@ -9,36 +9,23 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
+        <li><a>Home</a></li>
+      
         <li><a>Item 3</a></li>
       </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div> 
+    <img src={logo} className='w-16' alt="" srcset="" />
+   <p className='ml-16 text-2xl font-mono font-bold bg-gradient-to-r from-orange-500 via-green-600 to-yellow-400 inline-block text-transparent bg-clip-text'>Yummly Bakes</p>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul className="menu menu-horizontal px-1 text-xl">
+      <li><a>Home</a></li>
+  
+      <li><a>Recipes</a></li>
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className="ml-16">
+    <button className="btn text-xl text-white h-8 px-8 py-0 bg-gradient-to-r from-orange-500 to-white hover:from-amber-600 hover:to-yellow-500 ">Login</button>
   </div>
 </div>
     );
